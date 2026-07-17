5b303ffe1aa44ff703e5a3cb52d1538feb00eb25
Your Name <asihfibrahim600@gmail.com>
2026-07-17 20:45:33 +0530

    Initial commit

--- appointment.py
+++ appointment.py
@@ -1,1 +1,5 @@
 print("Hospital appointment system")
+print("Hospital appointment system")
+def register_patient(name ,age):
+  print(f"Patient {name} ,Age {age} registered successfully.")
+  register_patient("asif",20)
