f5f8658f6e88cfb673e43811ff775d282b10beb0
Asihf24mis0005 <asihfmohamad417@gmail.com>
2026-07-17 20:16:34 +0530

    Fix print statement in Appointment class

--- Hospital_Appointment_System.java
+++ Hospital_Appointment_System.java
@@ -1,5 +1,1 @@
-public class Appointment {
-    public static void main(String[] args) {
-        System.out.println("Hospital Appointment Booking System");
-    }
-}
+print("Hospital appointment system")
