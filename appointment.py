784683c249cec1a262b9fd0d74a9f1ba0d85a964
Asihf24mis0005 <asihfmohamad417@gmail.com>
2026-07-17 20:44:35 +0530

    Fix typo in function call for registering patient

--- appointment.py
+++ appointment.py
@@ -1,4 +1,4 @@
 print("Hospital appointment system")
 def register_patient(name ,age):
   print(f"Patient {name} ,Age {age} registered successfully.")
-  register_patien("asif",20)
+  register_patient("asif",20)
